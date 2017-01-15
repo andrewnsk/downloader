85ce16914eefa9c9c508800881e8ac270ff755c1
Smartgo <andrew@dorokhin.moscow>
2017-01-15 19:44:08 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='downloader',
+    version='0.0.1',
+    description='http file downloader',
+    author='Andrew Dorokhin (@andrewnsk)',
+    author_email='andrew@dorokhin.moscow',
+    url='http://github.com/andrewnsk/downloader',
+    packages=['downloader', 'tests'],
+    long_description="""\
+      http file downloader
+      """,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+    ],
+    package_data={
+        '': ['*.txt', '*.xsd']
+    },
+    keywords='download http',
+    license='MIT',
+    test_suite='tests'
+)
